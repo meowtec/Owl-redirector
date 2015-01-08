0d50b545d017f8bf1b819bc468a8b07122170b08
花漫 <zhubo.zb@alibaba-inc.com>
2015-01-08 17:45:49 +0800

    angular 输出 html

--- options/filter.js
+++ options/filter.js
@@ -1,5 +1,4 @@
-angular.module('owlApp', []).
-  filter('listFilter', function() {
+angular.module('owlApp', []).filter('listFilter', function() {
     return function(items) {
       var trashCount = 0;
       var returnList = [];
@@ -13,4 +12,10 @@ angular.module('owlApp', []).
       items.trashCount = trashCount;
       return returnList;
     }
-  });
+  }).filter(
+    'to_trusted', ['$sce', function ($sce) {
+      return function (text) {
+        return $sce.trustAsHtml(text);
+      }
+    }]
+  )

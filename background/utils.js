f34e272a73adfac293ad698844ecab80efd34d4f
花漫 <zhubo.zb@alibaba-inc.com>
2015-01-13 21:39:31 +0800

    精简 getEval

--- background/utils.js
+++ background/utils.js
@@ -1,11 +1,6 @@
-var getEval = (function(){
-  var getReturn = function (arg){
-    return arg
-  }
-  return function (str){
-    return eval('getReturn(' + str + ')')
-  }
-})()
+function getEval (str){
+  return eval('(' + str + ')')
+}
 function toDataUrl(data) {
   return 'data:text/html,' + encodeURIComponent(data)
 }

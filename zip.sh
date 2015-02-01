b59ad31dbba060e792e8e6582492ac83bcfcffc4
花漫 <zhubo.zb@alibaba-inc.com>
2015-02-01 22:53:31 +0800

    zip.sh

--- zip.sh
+++ zip.sh
@@ -1,3 +1,3 @@
 rm -rf ./owl-redirector/* owl-redirector.zip
-cp -r  _locales background options manifest.json owl.png ./owl-redirector/
+cp -r  _locales background options common manifest.json owl.png ./owl-redirector/
 7z a owl-redirector.zip ./owl-redirector/

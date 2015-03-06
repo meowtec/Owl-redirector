16ccbe159ae7c2e3311cac29058f91277b5d118f
花漫 <zhubo.zb@alibaba-inc.com>
2015-03-06 14:42:55 +0800

    修复默认数据bug

--- background/upgrade.js
+++ background/upgrade.js
@@ -10,7 +10,7 @@
   // 则写入 build，并且升级 ruleList，即将 regex 字段改为 urlType
   if (!globalSetting.build) {
     rules.forEach(function (rule) {
-      rule.urlType = rule.regex ? 'regex' : 'url'
+      rule.urlType = rule.urlType || (rule.regex ? 'regex' : 'url')
       delete rule.regex
     })
 
